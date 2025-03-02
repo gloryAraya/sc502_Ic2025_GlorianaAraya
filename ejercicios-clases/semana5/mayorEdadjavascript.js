6adf9ce71a0836acbb0d691e3b0d2aa4e683f539
gloryAraya <glorianaav.2@gmail.com>
2025-03-01 19:50:30 -0600

    ejercicios-clase
    ejercicios-clase

---
+++ ejercicios-clases/semana5/mayorEdadjavascript.js
@@ -0,0 +1,8 @@
+const age=17;
+if(age==18){
+    console.log('Soy mayor de edad');
+}else if (age<18){
+    console.log('Soy menor de edad')
+}else{
+    console.log('Sobrepaso la mayoría de edad')
+}
